5d27b7f5ca4b375a36fb09f1b3a3300eb484c18e
Rodney St Cloud <hoyrod1@aol.com>
2024-05-21 00:42:20 -0400

    First initial commit for Capestone Project. Created a package JSON file, installed express, nodemonthe and socket.io. and creates the  server in app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const http = require("http");
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+  console.log(`Listening on port: ${PORT}`);
+});
